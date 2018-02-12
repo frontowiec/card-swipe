57e46cc1c3e1bb4d359340ef8d7ac44aef7d6c8f
Marcin Sirocki <marcinsirocki@gmail.com>
2018-02-12 20:12:24 +0100

    with rxjs

--- index.js
+++ index.js
@@ -1,127 +1,75 @@
 (function () {
+    const {fromEvent, merge} = Rx.Observable;
 
     const body = document.getElementsByTagName('body')[0];
     const squares = document.querySelectorAll('.square');
 
-    let toggle = true;
+    const fromHammerEvent = event => [...squares].map(square => fromEvent(new Hammer(square), event));
 
-    let translate = 0;
-    let center = 1;
-    let canSwipeLeft = true;
-    let canSwipeRight = true;
+    const registeredSquaresTap$ = fromHammerEvent('tap');
+    const registeredSquaresSwipeLeft$ = fromHammerEvent('swipeleft');
+    const registeredSquaresSwipeRight$ = fromHammerEvent('swiperight');
 
-    squares.forEach(square => {
-        new Hammer(square)
-            .on('tap', scaleSquare)
-            .on('swipeleft swiperight', swipeSquare);
-    });
+    // SWIPE
 
-    function scaleSquare({target}) {
-        if (!target.classList.contains('center')) {
-            return;
-        }
+    const swipeLeft$ = merge(...registeredSquaresSwipeLeft$)
+        .mapTo(1);
 
-        toggle ? scaleUp(target) : scaleDown(target);
+    const swipeRight$ = merge(...registeredSquaresSwipeRight$)
+        .mapTo(-1);
 
-        toggle = !toggle;
-    }
+    const swipe$ = merge(swipeLeft$, swipeRight$);
 
-    function swipeSquare({type}) {
-        if (type === 'swipeleft' && canSwipeLeft) {
-            center++;
-            translate = translate - 324;
-            canSwipeRight = true;
+    const centerIndex$ = swipe$
+        .scan((acc, val) => {
+            const index = acc + val;
+            if (index < 0 || index > 2) {
+                return acc;
+            }
+            return index;
+        }, 1)
+        .distinctUntilChanged();
 
-            squares.forEach((squareEl, index) => {
-                if (index === center) {
-                    squareEl.classList.add('center');
-                    changeBodyBg(squareEl, index);
-                    blockSwipeOnEdge(index);
-                } else {
-                    squareEl.classList.remove('center');
-                }
+    const translate$ = centerIndex$.withLatestFrom(swipe$)
+        .map(val => val[1])
+        .scan((acc, val) => acc + (val * -324), 0)
+        .startWith(0);
 
-                swipeLeft(squareEl);
-            });
-        }
+    const translateTmp$ = swipe$.withLatestFrom(translate$)
+        .distinctUntilChanged(
+            (oldVal, newVal) => oldVal[0] === newVal[0] && oldVal[1] === newVal[1]
+        )
+        .scan((acc, val) => {
+            const position = val[0];
+            const translate = val[1];
+            return translate - (30 * position);
+        }, 0);
 
-        if (type === 'swiperight' && canSwipeRight) {
-            center--;
-            translate = translate + 324;
-            canSwipeLeft = true;
-
-            squares.forEach((squareEl, index) => {
-                if (index === center) {
-                    squareEl.classList.add('center');
-                    changeBodyBg(squareEl, index);
-                    blockSwipeOnEdge(index);
-                } else {
-                    squareEl.classList.remove('center');
-                }
-
-                swipeRight(squareEl);
-            });
-        }
-    }
-
-    const scaleUp = el => {
-        const translateX = getTranslateX(el);
-
-        el.style.zIndex = '9';
-        el.style.transform = `matrix(2.1, 0, 0, 2.1, ${translateX}, 0)`;
-
-        setTimeout(() => {
-            el.style.transform = `matrix(2, 0, 0, 2, ${translateX}, 0)`;
-        }, 250);
-    };
-
-    const scaleDown = el => {
-        const translateX = getTranslateX(el);
-
-        el.style.zIndex = '1';
-        el.style.transform = `matrix(.9, 0, 0, .9, ${translateX}, 0)`;
-
-        setTimeout(() => {
-            el.style.transform = `matrix(1, 0, 0, 1, ${translateX}, 0)`;
-        }, 250);
-    };
-
-    const swipeLeft = el => {
-        el.style.transform = `matrix(1, 0, 0, 1, ${translate - 30}, 0)`;
-
-        setTimeout(() => {
+    const swipe = (els, translate) => {
+        els.forEach(el => {
             el.style.transform = `matrix(1, 0, 0, 1, ${translate}, 0)`;
-        }, 200);
+        });
     };
 
-    const swipeRight = el => {
-        el.style.transform = `matrix(1, 0, 0, 1, ${translate + 30}, 0)`;
+    centerIndex$
+        .subscribe(index => {
+            markAsCenter(squares, index);
+            changeBodyBg(index);
+        });
 
-        setTimeout(() => {
-            el.style.transform = `matrix(1, 0, 0, 1, ${translate}, 0)`;
-        }, 200);
+    translate$
+        .debounceTime(200)
+        .subscribe(translate => swipe(squares, translate));
+
+    translateTmp$
+        .subscribe(translate => swipe(squares, translate));
+
+    const markAsCenter = (els, index) => {
+        els.forEach(el => el.classList.remove('center'));
+        els[index].classList.add('center');
     };
 
-    const blockSwipeOnEdge = index => {
-        // right edge
-        if (index === squares.length - 1) {
-            canSwipeLeft = false;
-        }
-
-        // left edge
-        if (index === 0) {
-            canSwipeRight = false;
-        }
-    };
-
-    const getTranslateX = (myElement) => {
-        // https://caniuse.com/#search=DOMMatrix
-        const style = window.getComputedStyle(myElement);
-        const matrix = new WebKitCSSMatrix(style.webkitTransform);
-        return matrix.m41;
-    };
-
-    const changeBodyBg = (el, index) => {
+    const changeBodyBg = (index, el) => {
         // todo: na podstawie koloru elementu
         switch (index) {
             case 0:
@@ -135,5 +83,40 @@
                 break;
         }
     };
+
+    // TAP
+
+    const taps$ = merge(...registeredSquaresTap$);
+
+    const toggle$ = taps$.scan(val => !val, false);
+
+    const centerSquare$ = taps$
+        .map(({target}) => target)
+        .filter(target => target.classList.contains('center'))
+        .withLatestFrom(toggle$, translate$)
+        .map(values => ({target: values[0], toggle: values[1], translateX: values[2]}))
+        .partition(val => val.toggle);
+
+    centerSquare$[0] // scaleUp$
+        .do(({target, translateX}) => {
+            target.style.zIndex = '9';
+            target.style.transform = `matrix(2.1, 0, 0, 2.1, ${translateX}, 0)`;
+        })
+        .debounceTime(250)
+        .do(({target, translateX}) => {
+            target.style.transform = `matrix(2, 0, 0, 2, ${translateX}, 0)`;
+        })
+        .subscribe();
+
+    centerSquare$[1] // scaleDown$
+        .do(({target, translateX}) => {
+            target.style.zIndex = '1';
+            target.style.transform = `matrix(.9, 0, 0, .9, ${translateX}, 0)`;
+        })
+        .debounceTime(250)
+        .do(({target, translateX}) => {
+            target.style.transform = `matrix(1, 0, 0, 1, ${translateX}, 0)`;
+        })
+        .subscribe();
 
 })();
